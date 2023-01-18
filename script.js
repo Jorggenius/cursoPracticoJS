fbc8fed1d033bcdb1aff79e38a6a6545962929ab
jorge luis <martinezmonajorgeluis@gmail.com>
2023-01-18 17:49:40 -0500

    Este es mi tercer commit en el presente repositorio cursoPracticoJS en el cual se puede ver el codigo de la clase # 11(ESCRIBIENDO HTML DESDE JAVASCRIPT)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const h1 = document.querySelector('h1')
 const p = document.querySelector('p')
 const parrafito = document.querySelector('.parrafito')
-const pid = document.querySelector('#pid')
+const pid = document.getElementById('pid')
 const input = document.querySelector('input')
 
 console.log(input.value)
@@ -14,3 +14,16 @@ console.log({
     input,
 })
 
+h1.innerHTML = 'Patito <br> Feo'
+h1.innerText = 'Patito <br> Feo'
+// console.log(h1.getAttribute("class"))
+// h1.setAttribute("class", "rojo")
+h1.classList.add('rojo')
+h1.classList.remove('verde')
+// h1.classList.toggle('verde')
+// h1.classList.contains('verde')
+input.value = "456"
+const img = document.createElement('img')
+img.setAttribute('src', 'https://www.grammar-monster.com/images/grammarMonsterEye.png')
+pid.innerHTML = ''
+pid.append(img)
